71009dd69603a1f90bd6cb6524f9d8898160e136
Алексей <yudakov1814@gmail.com>
2022-03-11 15:17:22 +0700

    add lab1

--- WebLabs/Startup.cs
+++ WebLabs/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebLabs.Services;
 
 namespace WebLabs
 {
@@ -24,6 +25,7 @@ namespace WebLabs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<CalcService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
